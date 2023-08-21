b8d9a2d9c9101d00b684f98165f9753880f411e4
jeff bennington <jeff.tflearn@gmail.com>
2023-08-21 10:12:39 +0800

    新增post get http方法注解

--- src/main/java/com/to/core/ann/Get.java
+++ src/main/java/com/to/core/ann/Get.java
@@ -1,11 +1,11 @@
-package com.example.lib;
+package com.to.core.ann;
 
 import java.lang.annotation.*;
 
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-public @interface Post {
-  public String httpMethod() default "post";
+public @interface Get {
+  public String httpMethod() default "get";
   public String path() default "";
 }
--- src/main/java/com/to/core/ann/Post.java
+++ src/main/java/com/to/core/ann/Post.java
@@ -1,11 +1,12 @@
-package com.example.lib;
+package com.to.core.ann;
 
 import java.lang.annotation.*;
 
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-public @interface Get {
-  public String httpMethod() default "get";
+public @interface Post {
+  public String httpMethod() default "post";
   public String path() default "";
 }
+
